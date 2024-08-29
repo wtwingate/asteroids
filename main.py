a8a48f9d24f5f7baa9f72b86e0bed95c9b55c1da
William Wingate <wtwingate@gmail.com>
2024-08-29 10:09:50 -0400

    Set up game clock at 60 FPS

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import pygame
 
 from constants import *
 
+
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    delta_time = 0
+
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -14,6 +19,10 @@ def main():
 
         screen.fill(color=(0, 0, 0))
         pygame.display.flip()
+
+        elapsed_time = clock.tick(60)
+        delta_time += elapsed_time / 1000
+
 
 if __name__ == "__main__":
     main()
